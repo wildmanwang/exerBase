a4e33fe80e64bd91d5417c090e532e605b8469ca
Cliff Wang <cliff.w@qq.com>
2018-12-11 18:11:07 +0800

    started

---
+++ courseSelection/common.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+通用工具
+"""
+
+__author__ = 'Cliff Wang'
--- courseSelection/student.py
+++ courseSelection/student.py
@@ -1,9 +1,17 @@
 # -*- coding:utf-8 -*-
 """
-学生
+学生：编码、姓名、性别、出生日期、注册日期
 
-报名
-查看档案
+{
+    "1":    "注册--register",
+    "2":    "登录--login",
+    "21":       "选择学校--selectSchool",
+    "211":          "查看可报名课程--showSubject",
+    "213":          "查看我的班级--showGrade",
+    "22":       "修改密码--password",
+    "20"        "退出登录--logout",
+    "0":    "退出--exit"
+}
 """
 __author__ = "Cliff.wang"
 
